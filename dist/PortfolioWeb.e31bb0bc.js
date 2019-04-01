3a789fe018dfe359dbec772c72c4eacf66de0f43
= <=>
2019-03-31 20:09:21 -0400

    Modify company image

--- dist/PortfolioWeb.e31bb0bc.js
+++ dist/PortfolioWeb.e31bb0bc.js
@@ -15737,7 +15737,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60204" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54116" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
